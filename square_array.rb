8c5c0036d7ca2d97acf1fb0e21fb32ea691c5d5b
Ricky dildy <rdildy92@gmail.com>
2019-11-09 00:41:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,19 +1,19 @@
 def square_array(array)
   numbers = [1,2,3]
-  expect(numbers).to_not receive(:collect)
-  expect(numbers).to_not receive(:map)
-  expect(numbers).to_not receive(:inject)
+  (numbers).to_not receive(:collect)
+  (numbers).to_not receive(:map)
+  (numbers).to_not receive(:inject)
   square_array(numbers)
   
 end 
 
 'calls on each' do 
   numbers = [1,2,3]
-  expect(numbers).to receive(:each)
+  (numbers).to receive(:each)
   square_array(numbers)
 end 
 
 'should square the elements in an array' do
-    expect(square_array([1,2,3])).to eq([1,4,9])
+    (square_array([1,2,3])).to eq([1,4,9])
     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
   end
